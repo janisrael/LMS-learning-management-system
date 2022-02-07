e12d909ea0bd057146d5ca585b51b2135f7a6ccf
jan_israel <jan.israel@smartchartsfx.com>
2022-02-08 01:13:29 +0800

    fix default route on content

--- admin/resources/js/components/Layout/MainWrapperComponent.vue
+++ admin/resources/js/components/Layout/MainWrapperComponent.vue
@@ -1,37 +1,43 @@
 <template>
   <div>
     <aside-component></aside-component>
-    <content-component></content-component>
+    <content-component :routes="new_routes"></content-component>
   </div>
 </template>
 
 <script>
-import AsideComponent from './AsideComponent'
-import ContentComponent from './ContentComponent'
-  export default {
-    name: 'MainWrapperComponent',
-    components: {
-      AsideComponent,
-      ContentComponent
+import AsideComponent from "./AsideComponent";
+import ContentComponent from "./ContentComponent";
+export default {
+  name: "MainWrapperComponent",
+  components: {
+    AsideComponent,
+    ContentComponent,
+  },
+  data() {
+    return {
+      loading: false,
+      new_routes: [],
+      routes: this.$routers,
+    };
+  },
+  computed: {},
+  created() {
+    this.mapRoutes();
+  },
+  methods: {
+    mapRoutes() {
+      let new_route = [];
+      this.routes.forEach((value, i) => {
+        if (value.type === "page") {
+          new_route.push(value);
+        }
+      });
+      this.new_routes = new_route;
     },
-    data() {
-      return {
-        loading: false,
-  
-      }
-    },
-    computed: {
-
-    },
-    created () {
-
-    },
-    methods: {
-     
-    },
-  }
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
